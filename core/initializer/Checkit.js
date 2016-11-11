bfad81197009279ee3d57d33720ad44ab621466a
An Nguyen <nguyenhuuan@gmail.com>
2016-11-11 13:20:01 +0700

    Update checkit module.

--- core/initializer/Checkit.js
+++ core/initializer/Checkit.js
@@ -1,29 +1,37 @@
 var logger = require('log4js').getLogger('Init.Checkit');
 
 module.exports = function(Checkit, dirs) {
+  var subs = ['validator', 'transformer'];
+  _.forEach(dirs, function(d) {
+    _.forEach(subs, function(sub) {
+      let dir = path.join(d, sub);
 
-  _.forEach(dirs, function(dir) {
-    logger.info('Initializer::Checkit dir=' + dir);
-    if (!FileUtils.isDirectorySync(dir)) {
-      throw new Error('Invalid service directory: ' + dir);
-    }
-
-    var files = FileUtils.listFiles(dir, /.js$/i);
-    if (!files.length) {
-      logger.warn('Checkit directory (' + dir + ') is empty');
-      return;
-    }
-
-    _.forEach(files, function(file) {
-      if (!FileUtils.isFileSync(file)) {
-        throw new Error('Invalid checkit file: ' + file);
+      logger.info('Initializer::Checkit dir=' + dir);
+      if (!FileUtils.isDirectorySync(dir)) {
+        throw new Error('Invalid checkit directory: ' + dir);
       }
 
-      var module = require(file);
-      name = path.basename(file, '.js');
-      Checkit.Validator.prototype[name] = module;
+      var files = FileUtils.listFiles(dir, /.js$/i);
+      if (!files.length) {
+        logger.warn('Checkit directory (' + dir + ') is empty');
+        return;
+      }
 
-      logger.info('initialized customized validator: ' + name);
+      _.forEach(files, function(file) {
+        if (!FileUtils.isFileSync(file)) {
+          throw new Error('Invalid checkit file: ' + file);
+        }
+
+        var module = require(file);
+        name = path.basename(file, '.js');
+        if (sub === 'validator') {
+          Checkit.Validator.prototype[name] = module;
+        } else if (sub === 'transformer') {
+          Checkit.Transformer.prototype[name] = module;
+        }
+
+        logger.info('initialized customized validator: ' + name);
+      });
     });
 
   });
