fac0b45b5d3d080f5c45a761dbd939879d0b6035
An Nguyen <nguyenhuuan@gmail.com>
2016-12-29 02:46:42 +0700

    Re-arrange controllers.

--- core/controller/FacebookController.js
+++ core/controller/FacebookController.js
@@ -7,6 +7,20 @@ var request         = require('request');
 module.exports = BaseController.extends({
   classname: 'FacebookController',
 
+  login: function(req, res) {
+    var self = this;
+    var [err, params] = new Checkit({
+      fb_access_token: ['required', 'string'],
+    }).validateSync(req.allParams);
+
+    if (err) {
+      return res.badRequest(err.toString());
+    }
+
+    var AuthService = req.getService('AuthService');
+    AuthService.getUserFacebook(params.fb_access_token, this.ok.bind(this, req, res));
+  },
+
   link: function(req, res) {
     var self = this;
     var [err, params] = new Checkit({
@@ -25,7 +39,11 @@ module.exports = BaseController.extends({
   },
 
   unlink: function(req, res) {
-    // TODO
+    var self = this;
+    var userId = req.user.id;
+
+    var UserFacebookModel = req.getModel('UserFacebookModel');
+    UserFacebookModel.remove(userId, this.deleted.bind(this, req, res));
   },
 
 });
--- core/controller/TwitterController.js
+++ core/controller/TwitterController.js
@@ -7,6 +7,21 @@ var request         = require('request');
 module.exports = BaseController.extends({
   classname: 'TwitterController',
 
+  login: function(req, res) {
+    var self = this;
+    var [err, params] = new Checkit({
+      tokenKey: ['required', 'string'],
+      tokenSecret: ['required', 'string'],
+    }).validateSync(req.allParams);
+
+    if (err) {
+      return res.badRequest(err.toString());
+    }
+
+    var AuthService = req.getService('AuthService');
+    AuthService.getUserTwitter(params.tokenKey, params.tokenSecret, this.ok.bind(this, req, res));
+  },
+
   link: function(req, res) {
     var self = this;
     var [err, params] = new Checkit({
@@ -26,7 +41,38 @@ module.exports = BaseController.extends({
   },
 
   unlink: function(req, res) {
-     // TODO
+    var self = this;
+    var userId = req.user.id;
+
+    var UserTwitterModel = req.getModel('UserTwitterModel');
+    UserTwitterModel.remove(userId, this.deleted.bind(this, req, res));
+  },
+
+  login2: function(req, res, next) {
+    passport.authenticate('twitter')(req, res, next);
+  },
+
+  login2CB: function(req, res) {
+    var self = this;
+    var AuthService = req.getService('AuthService');
+
+    passport.authenticate('twitter', {
+      failureRedirect: '/login'
+    }, function(err, data, info) {
+      if (err) {
+        return res.sendError(err);
+      }
+
+      if (data === false) {
+        if (info && info.message) {
+          return res.sendError(info.message);
+        }
+
+        return res.sendError(info);
+      }
+
+      AuthService.getUserTwitter2(data.profile, self.ok.bind(self, req, res));
+    })(req, res);
   },
 
 });
