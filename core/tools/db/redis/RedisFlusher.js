489f3a25c769845804cd14c2601d0930c8d17bfd
An Nguyen <nguyenhuuan@gmail.com>
2017-01-11 16:09:15 +0700

    Add reds flushing tool.

---
+++ core/tools/db/redis/RedisFlusher.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+var redis     = require('redis');
+var logger    = require('log4js').getLogger('RedisFlusher');
+
+var client = redis.createClient({
+  host: process.env.REDIS_SERVER_ADDRESS,
+  port: process.env.REDIS_SERVER_PORT,
+});
+
+client.flushdb(function (err, succeeded) {
+  if (err) {
+    logger.error('Something went wrong when flushing redis db: ' + err);
+  }
+  console.log('Finished flushing redis: ' + succeeded);
+  process.exit(0);
+});
