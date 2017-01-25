198621fa81f2b6b9eca0eed26afa14466f773036
An Nguyen <nguyenhuuan@gmail.com>
2017-01-26 01:17:24 +0700

    Get age range from Facebook.

--- core/service/FacebookService.js
+++ core/service/FacebookService.js
@@ -10,11 +10,15 @@ module.exports = SocialNetworkService.extends({
   },
 
   getUserDefFromInfo: function(fbInfo) {
+    var age = fbInfo.age_range ? (fbInfo.age_range.max || fbInfo.age_range.min) : 0;
+    var birthYear = new Date().getFullYear() - age;
+
     return {
       username: fbInfo.id,
       email: fbInfo.email || fbInfo.id,
       full_name: fbInfo.first_name + ' ' + fbInfo.last_name,
       avatar_url: fbInfo.picture.data.url,
+      birthday: birthYear + '0101',
     };
   },
 
@@ -22,7 +26,7 @@ module.exports = SocialNetworkService.extends({
     var secret = process.env.FACEBOOK_APP_SECRET;
     var hash = crypto.createHmac('sha256', secret).update(fbAcessToken);
     var appsecretProof = hash.digest('hex');
-    var fields = 'id,name,email,first_name,last_name,gender,link,picture.type(large)';
+    var fields = 'id,name,email,first_name,last_name,gender,link,picture.type(large),age_range';
     var url = util.format(
       'https://graph.facebook.com/v2.8/me?fields=%s&access_token=%s&appsecret_proof=%s',
       fields, fbAcessToken, appsecretProof
