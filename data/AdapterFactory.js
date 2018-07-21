d02049cb8c3ce8851e0c3e5970949b76b83c3187
An Nguyen <nguyenhuuan@gmail.com>
2018-07-22 02:21:27 +0700

    Support BN in mysql

--- data/AdapterFactory.js
+++ data/AdapterFactory.js
@@ -83,7 +83,9 @@ module.exports = Class.singleton({
         connectionLimit: config.connectionLimit || 10,
         waitForConnections: config.waitForConnections || true,
         queueLimit: config.queueLimit || 5,
-        charset: 'utf8mb4_general_ci'
+        charset: 'utf8mb4_general_ci',
+        supportBigNumbers: config.supportBigNumbers,
+        bigNumberStrings: config.bigNumberStrings,
       });
     } else if (type === Const.DATA_SOURCE_TYPE.MONGODB) {
       pool = {};
